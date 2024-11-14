8745765079a96d9ab9e060af6f1b765cb079c59c
slickw <723012@student.dusd.net>
2024-11-14 14:54:38 -0800

    added collectables

---
+++ 2DBeginnerAdventureP8Gallardo/Assets/Scripts/HealthCollectable.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCollectable : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Object that entered the trigger is: " + other);
+    }
+}
--- 2DBeginnerAdventureP8Gallardo/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP8Gallardo/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 3.0f;
+    public int maxHealth = 5;
+    int currentHealth;
+
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
@@ -11,6 +16,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
 
 
 
@@ -29,12 +35,20 @@ public class PlayerController : MonoBehaviour
 
         void FixedUpdate()
         {
-            Vector2 position = rigidbody2d.position;
-            position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-            position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        Vector2 position = rigidbody2d.position;
+            position.x = position.x + speed * horizontal *Time.deltaTime;
 
-            rigidbody2d.MovePosition(position);
+            position.y = position.y + speed * vertical * Time.deltaTime;
+
+
+
+
+        rigidbody2d.MovePosition(position);
 
         }
-
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth +"/" + maxHealth);
+    }
     }
