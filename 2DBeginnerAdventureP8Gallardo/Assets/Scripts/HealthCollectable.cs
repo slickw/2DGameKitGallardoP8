4e87584ef7170c24e46cea3075492e5f5a820127
slickw <723012@student.dusd.net>
2024-11-14 15:25:03 -0800

    I finished collectables
    Finished collectables and going to do damagables now

--- 2DBeginnerAdventureP8Gallardo/Assets/Scripts/HealthCollectable.cs
+++ 2DBeginnerAdventureP8Gallardo/Assets/Scripts/HealthCollectable.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class HealthCollectable : MonoBehaviour
 {
-    void OnTriggerEnter2D(Collider2D other)
+   void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Object that entered the trigger is: " + other);
+        PlayerController controller = other.GetComponent<PlayerController>();
+        if (controller != null)
+        {
+            if(controller.ChangeHealth < controller.maxHealth)
+            {
+
+            }
+    
+        }
+        
     }
 }
--- 2DBeginnerAdventureP8Gallardo/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP8Gallardo/Assets/Scripts/PlayerController.cs
@@ -17,6 +17,7 @@ public class PlayerController : MonoBehaviour
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        
 
 
 
@@ -46,7 +47,8 @@ public class PlayerController : MonoBehaviour
         rigidbody2d.MovePosition(position);
 
         }
-    void ChangeHealth(int amount)
+  
+   public void ChangeHealth(int amount)
     {
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth +"/" + maxHealth);
